cd556bdf9d8ec05e0ac8ea244519f90a119eb5b7
Zaidashraf19 <muhammadzaidmuhammadashraf@gmail.com>
2025-05-20 21:00:41 +0500

    Icons edits & about pages more created

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react"
+import { AiOutlineFundProjectionScreen, AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { CgFileDocument } from "react-icons/cg";
 import { Link } from "react-router-dom"
 
 const nav = () => {
@@ -18,27 +20,27 @@ const nav = () => {
                     <ul className="flex flex-col md:flex-row gap-5">
 
                         <div className="flex hover:underline underline-offset-2">
-                            <svg className="w-[25px] h-[25px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5" />
-                            </svg>
+                            <span className="text-[25px]">
+                                <AiOutlineHome />
+                            </span>
                             <Link to={"/"}>Home</Link>
                         </div>
                         <div className="flex hover:underline underline-offset-2">
-                            <svg className="w-[25px] h-[25px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" strokeWidth="2" d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                            </svg>
+                            <span className="text-[25px]">
+                                <AiOutlineUser />
+                            </span>
                             <Link to={"/about"}>About</Link>
                         </div>
                         <div className="flex hover:underline underline-offset-2">
-                            <svg class="w-[25px] h-[25px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4" />
-                            </svg>
+                            <span className="text-[25px]">
+                                <AiOutlineFundProjectionScreen />
+                            </span>
                             <Link to={"/project"}>Projects</Link>
                         </div>
                         <div className="flex hover:underline underline-offset-2">
-                            <svg class="w-[25px] h-[25px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 10.5h.01m-4.01 0h.01M8 10.5h.01M5 5h14a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1h-6.6a1 1 0 0 0-.69.275l-2.866 2.723A.5.5 0 0 1 8 18.635V17a1 1 0 0 0-1-1H5a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z" />
-                            </svg>
+                            <span className="text-[25px]">
+                                < CgFileDocument />
+                            </span>
                             <Link to={"/contact"}>Contact Us</Link>
                         </div>
 
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,4 +1,9 @@
+import { RiNextjsFill, RiTailwindCssFill } from 'react-icons/ri';
 import ME from '../assets/me.jpeg'
+import { FaCss3, FaHtml5, FaReact } from "react-icons/fa";
+import { DiGit, DiJavascript1 } from 'react-icons/di';
+import { SiFirebase, SiRedux } from 'react-icons/si';
+
 
 const About = () => {
     return (
@@ -35,40 +40,70 @@ const About = () => {
             <div className='flex justify-center gap-10 flex-wrap my-5 mx-30'>
                 {/* HTML */}
                 <div className='p-20 border border-white rounded w-50 shadow-xl hover:transform-gpu'>
-                    <svg class="w-[48px] h-[48px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                        <path d="m3 2 1.578 17.824L12 22l7.467-2.175L21 2H3Zm14.049 6.048H9.075l.172 2.016h7.697l-.626 6.565-4.246 1.381-4.281-1.455-.288-2.932h2.024l.16 1.411 2.4.815 2.346-.763.297-3.005H7.416l-.562-6.05h10.412l-.217 2.017Z" />
-                    </svg>
+                    <span className='text-[48px]'>
+                        <FaHtml5 />
+                    </span>
                 </div>
 
                 {/* CSS */}
                 <div className='p-20 border border-white rounded w-50 shadow-xl hover:transform-gpu '>
-                    <svg class="w-[48px] h-[48px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                        <path d="m3 2 1.578 17.834L12 22l7.468-2.165L21 2H3Zm13.3 14.722-4.293 1.204H12l-4.297-1.204-.297-3.167h2.108l.15 1.526 2.335.639 2.34-.64.245-3.05h-7.27l-.187-2.006h7.64l.174-2.006H6.924l-.176-2.006h10.506l-.954 10.71Z" />
-                    </svg>
+                    <span className='text-[48px]'>
+                        <FaCss3 />
+                    </span>
                 </div>
 
                 {/* TAILWIND */}
                 <div className='p-20 border border-white rounded w-50 shadow-xl hover:transform-gpu '>
-                    <svg class="w-[48px] h-[48px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                        <path d="M11.782 5.72a4.773 4.773 0 0 0-4.8 4.173 3.43 3.43 0 0 1 2.741-1.687c1.689 0 2.974 1.972 3.758 2.587a5.733 5.733 0 0 0 5.382.935c2-.638 2.934-2.865 3.137-3.921-.969 1.379-2.44 2.207-4.259 1.231-1.253-.673-2.19-3.438-5.959-3.318ZM6.8 11.979A4.772 4.772 0 0 0 2 16.151a3.431 3.431 0 0 1 2.745-1.687c1.689 0 2.974 1.972 3.758 2.587a5.733 5.733 0 0 0 5.382.935c2-.638 2.933-2.865 3.137-3.921-.97 1.379-2.44 2.208-4.259 1.231-1.253-.673-2.19-3.443-5.963-3.317Z" />
-                    </svg>
+                    <span className="text-[48px]">
+                        <RiTailwindCssFill />
+                    </span>
                 </div>
 
-                {/*  */}
+                {/* JAVASCRIPT */}
                 <div className='p-20 border border-white rounded w-50 shadow-xl hover:transform-gpu'>
+                    <span className='text-[48px]'>
+                        <DiJavascript1 />
+                    </span>
                 </div>
 
-
-
+                {/* REACT JS */}
                 <div className='p-20 border border-white rounded w-50 shadow-xl hover:transform-gpu '>
+                    <span className='text-[48px]'>
+                        <FaReact />
+                    </span>
                 </div>
 
-
-
+                {/* REDUX */}
                 <div className='p-20 border border-white rounded w-50 shadow-xl hover:transform-gpu '>
+                    <span className='text-[48px]'>
+                        <SiRedux />
+                    </span>
                 </div>
 
-                
+
+                {/* NEXT JS */}
+                <div className='p-20 border border-white rounded w-50 shadow-xl hover:transform-gpu '>
+                    <span className='text-[48px]'>
+                        <RiNextjsFill />
+                    </span>
+                </div>
+
+                {/* FIREBASE */}
+                <div className='p-20 border border-white rounded w-50 shadow-xl hover:transform-gpu '>
+                    <span className='text-[48px]'>
+                        <SiFirebase />
+                    </span>
+                </div>
+
+                {/* GIT */}
+                <div className='p-20 border border-white rounded w-50 shadow-xl hover:transform-gpu '>
+                    <span className='text-[48px]'>
+                        <DiGit />
+                    </span>
+                </div>
+
+
+
             </div>
         </>
     )
