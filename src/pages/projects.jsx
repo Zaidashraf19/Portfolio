899cbc8fd6f05fd5863ab0b7da5444b05ecfb416
Zaidashraf19 <muhammadzaidmuhammadashraf@gmail.com>
2025-06-03 11:31:41 +0500

    Project Ui edit

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -5,8 +5,8 @@ const Project = () => {
     return (
         <>
             <h2 className="text-center font-bold uppercase text-2xl">My Recent Work</h2>
-            <p className="text-center text-lg">Here are few works I've worked on recently</p>
-            <div className="flex justify-center gap-10 flex-wrap my-5 mx-30">
+            <p className="text-center">Here are few works I've worked on recently</p>
+            <div className="flex justify-center gap-10 flex-wrap my-5 mx-5">
                 <div className="bg-[#181a27] shadow-2xl shadow-black w-90 p-2">
                     <img
                         src={BloggingApp}
@@ -14,7 +14,7 @@ const Project = () => {
                         className='rounded'
                     />
                     <h3 className='font-semibold text-lg text-center'>Blogging App</h3>
-                    <p>I've created a blogging app where the user can add and delete post and cn also upload image</p>
+                    <p className='text-center'>I've created a blogging app where the user can add and delete post and cn also upload image</p>
                     {/* <span className="bg-[#181a27] rounded-lg p-2 text-xl">
                         <a href="">
                             <button className='text-[25px]'>
